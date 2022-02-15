97c14c17d38189f54442aa5e5cc179563a91b9ee
khanisha123 <khanisha@code.edu.az>
2022-02-16 03:52:52 +0400

    Add project files.

---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data.DAL;
+using WebApi.Data.Entity;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly Context _context;
+        public ProductController(Context context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public List<Product> Get() 
+        {
+            return _context.Products.ToList();
+        }
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetOne(int id) 
+        {
+            
+            Product product = _context.Products.FirstOrDefault(x=>x.Id == id);
+            if (product == null)
+            {
+                return StatusCode(404);
+            }
+            return StatusCode(200,product);
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Product product)
+        {
+
+            Product dbproduct = _context.Products.FirstOrDefault(x=>x.Id == product.Id);
+            if (dbproduct ==null)
+            {
+                return NotFound();
+            }
+            dbproduct.Name = product.Name;
+            dbproduct.Price = product.Price;
+            _context.SaveChanges();
+            return StatusCode(200);
+
+
+        }
+
+    }
+}
---
+++ WebApi/Data/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data.Entity;
+
+namespace WebApi.Data.DAL
+{
+    public class Context :DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
